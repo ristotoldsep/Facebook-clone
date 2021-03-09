5ddd87a26ed837b09496236ed1b5b4c7ec040cad
Risto Tõldsep <r.toldsep@gmail.com>
2021-03-09 23:33:01 +0200

    Started building the app

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+$con = mysqli_connect('localhost', 'root', '', 'social');
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+$query = mysqli_query($con, "INSERT INTO test(name) VALUES ('Ricoboy')");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facebook</title>
+</head>
+<body>
+    
+</body>
+</html>
