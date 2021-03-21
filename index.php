673b8e1bb39c544bb2fac2be78ab8317cf1ab23b
Risto Tõldsep <r.toldsep@gmail.com>
2021-03-21 03:25:42 +0200

    Created registration form & db connection

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (mysqli_connect_errno()) {
     echo "Failed to connect: " . mysqli_connect_errno();
 }
 
-$query = mysqli_query($con, "INSERT INTO test(name) VALUES ('Ricoboy')");
+//$query = mysqli_query($con, "INSERT INTO test(name) VALUES ('Ricoboy')");
 
 ?>
 
