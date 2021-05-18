eeb22f287838ea7c1f0030764bd39eb3b4444c49
Risto Tõldsep <r.toldsep@gmail.com>
2021-05-19 01:54:08 +0300

    Added local and production db connection logic

--- config/config.php
+++ config/config.php
@@ -4,7 +4,26 @@ session_start(); //Starts the session
 
 $timezone = date_default_timezone_set("Europe/Helsinki");
 
-$con = mysqli_connect('localhost', 'root', '', 'social');
+//IF DEVELOPMENT ENVIRONMENT - use local DB
+if (strstr($_SERVER['SERVER_NAME'], 'localhost')) {
+    $con = mysqli_connect('localhost', 'root', '', 'social');
+}
+else { //IF PRODUCTION ENVIRONMENT - Use DB from heroku
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"], 1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+
+    // Connect to DB
+    $con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+}
+
+// Change character set to utf8
+mysqli_set_charset($con, "utf8");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect: " . mysqli_connect_errno();
