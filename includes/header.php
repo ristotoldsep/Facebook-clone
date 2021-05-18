e29fa6d4de5cbd074e7976c4e702a0762a0a01b4
Risto Tõldsep <r.toldsep@gmail.com>
2021-05-19 02:52:14 +0300

    Small improvements

--- includes/header.php
+++ includes/header.php
@@ -37,7 +37,7 @@ if (isset($_SESSION['username'])) {
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="assets/js/bootbox.min.js"></script>
     <script src="assets/js/facebook.js"></script>
-    <script src="assets/js/jquery.jcrop.js"></script>
+    <script src="assets/js/jquery.Jcrop.js"></script>
     <script src="assets/js/jcrop_bits.js"></script>
 
     <!-- CSS -->
