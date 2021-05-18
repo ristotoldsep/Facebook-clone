62368eaf9ede6ceebc35f4845d730f63e4e914b7
Risto Tõldsep <r.toldsep@gmail.com>
2021-05-19 02:10:40 +0300

    Small improvements

--- includes/header.php
+++ includes/header.php
@@ -26,6 +26,7 @@ if (isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-512.png">
     <title>Facebook</title>
 
     <!-- Javascript -->
