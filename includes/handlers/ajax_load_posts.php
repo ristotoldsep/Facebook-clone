f81fc8edb32227a1facdc29141d8ca8e155647bc
Risto Tõldsep <r.toldsep@gmail.com>
2021-05-19 02:21:47 +0300

    Small improvements

--- includes/handlers/ajax_load_posts.php
+++ includes/handlers/ajax_load_posts.php
@@ -4,7 +4,7 @@ include("../../config/config.php");
 include("../classes/User.php");
 include("../classes/Post.php");
 
-$limit = 10; //Nr of posts to be loaded per call
+$limit = 5; //Nr of posts to be loaded per call
 
 $posts = new Post($con, $_REQUEST['userLoggedIn']);
 $posts->loadPostsFriends($_REQUEST, $limit);
